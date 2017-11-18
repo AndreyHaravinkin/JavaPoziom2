b0958a58a8e935777905745d54c77e79a69db4ca
Andrey Kharavinkin <andreyharavinkin@gmail.com>
2017-11-18 10:31:27 +0100

    Builder

---
+++ src/main/java/pl/sda/javaPoziom2/wzorceProektowe/Main.java
@@ -0,0 +1,15 @@
+package pl.sda.javaPoziom2.wzorceProektowe;
+
+public class Main {
+    public static void main(String[] args) {
+        builder();
+    }
+    private  static void builder(){
+        User user1 = new User.UserBuilder("Anna").lastname("Nowak").age(12).adress("Lodz").phone("1234").build();
+
+        User user2 = new User.UserBuilder("Jan").lastname("Kowalski").build();
+
+        System.out.println(user1.toString());
+        System.out.println(user2.toString());
+    }
+}
---
+++ src/main/java/pl/sda/javaPoziom2/wzorceProektowe/User.java
@@ -0,0 +1,85 @@
+package pl.sda.javaPoziom2.wzorceProektowe;
+
+public class User {
+    private String firstname;
+    private String lastname;
+    private String adress;
+    private int age;
+    private String phone;
+
+    private User(UserBuilder userBuilder) {
+        this.firstname = userBuilder.firstname;
+        this.lastname = userBuilder.lastname;
+        this.age = userBuilder.age;
+        this.adress = userBuilder.adress;
+        this.phone = userBuilder.phone;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", adress='" + adress + '\'' +
+                ", age=" + age +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
+
+    /*************************************/
+    /*************************************/
+
+    static class UserBuilder{
+        private String firstname;
+        private String lastname;
+        private String adress;
+        private int age;
+        private String phone;
+
+        public UserBuilder  (String firstname) {
+            this.firstname = firstname;
+        }
+
+        public UserBuilder lastname(String lastname){
+            this.lastname = lastname;
+            return this;
+        }
+
+        public UserBuilder age(int age){
+            this.age = age;
+            return this;
+        }
+
+        public UserBuilder adress(String adress){
+            this.adress = adress;
+            return this;
+        }
+
+
+        public UserBuilder phone(String phone){
+            this.phone = phone;
+            return this;
+        }
+
+        public User build(){
+            User user = new User(this);
+            return user;
+        }
+    }
+}
